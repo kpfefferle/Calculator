f27c22a66e470f650a508dbc84129f0dab187bef
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-04 17:51:06 -0400

    Create Graph VC with split-view navigation

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Kevin Pfefferle on 6/4/16.
+//  Copyright © 2016 Kevin Pfefferle. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+}
