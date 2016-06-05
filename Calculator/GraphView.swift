c02215b0f18ebdfd9eb59790b24fe5dba1a4c556
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-05 18:37:55 -0400

    Create GraphView

---
+++ Calculator/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Calculator
+//
+//  Created by Kevin Pfefferle on 6/5/16.
+//  Copyright © 2016 Kevin Pfefferle. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
