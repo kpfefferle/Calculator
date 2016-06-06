5daa9c45cf866ecaf9331a103166a8277d75ac84
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-05 20:25:09 -0400

    Draw graph for set program

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -10,13 +10,71 @@ import UIKit
 
 class GraphView: UIView {
 
-    override func drawRect(rect: CGRect) {
+    private let brain = CalculatorBrain()
 
-        let origin = CGPoint(x: bounds.midX, y: bounds.midY)
+    var color = UIColor.redColor()
+    var lineWidth: CGFloat = 1.0
+    var origin: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    var scale: CGFloat = 50.0 // points per unit
+    var program: CalculatorBrain.PropertyList {
+        get {
+            return brain.program
+        }
+        set {
+            brain.program = newValue
+        }
+    }
 
+    private func drawAxes() {
         let axesDrawer = AxesDrawer(contentScaleFactor: contentScaleFactor)
-        axesDrawer.drawAxesInRect(bounds, origin: origin, pointsPerUnit: contentScaleFactor)
+        axesDrawer.drawAxesInRect(bounds, origin: origin, pointsPerUnit: scale)
+    }
 
+    private func valueForXCoordinate(xCoord: CGFloat) -> Double {
+        return Double((xCoord - origin.x) / scale)
+    }
+
+    private func coordinateForYValue(yValue: Double) -> CGFloat {
+        return origin.y - (CGFloat(yValue) * scale)
+    }
+
+    private func yForX(xCoord: CGFloat) -> CGFloat {
+        brain.variableValues["M"] = valueForXCoordinate(xCoord)
+        return coordinateForYValue(brain.result)
+    }
+
+    private func drawGraph() {
+
+        let path = UIBezierPath()
+        var x = bounds.minX
+        var y = yForX(x)
+        path.moveToPoint(CGPoint(x: x, y: y))
+
+        while x < bounds.maxX {
+
+            let newX = x + (1 / contentScaleFactor)
+            let newY = yForX(newX)
+
+            if abs(newY - y) > max(bounds.width, bounds.height) {
+                path.moveToPoint(CGPoint(x: newX, y: newY))
+            } else {
+                path.addLineToPoint(CGPoint(x: newX, y: newY))
+            }
+
+            x = newX
+            y = newY
+        }
+
+        color.setStroke()
+        path.lineWidth = lineWidth
+        path.stroke()
+    }
+
+    override func drawRect(rect: CGRect) {
+        drawAxes()
+        drawGraph()
     }
 
 }
